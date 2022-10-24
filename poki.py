05bea11b9af5253525ba3e326dfe6d6943970d59
Luis Peralta <lperaltazx78@gmail.com>
2022-10-24 15:58:46 -0400

    first commit

---
+++ poki.py
@@ -0,0 +1,6 @@
+def getpokemoninfo(charmander):
+url = f'https://pokeapi.co/api/v2/{pokemon}/charmander'
+response = r.get(url)
+if response.ok:
+    my_dict = response.co
+    pokemon_dict = {https://pokeapi.co/api/v2/ability/66/}
